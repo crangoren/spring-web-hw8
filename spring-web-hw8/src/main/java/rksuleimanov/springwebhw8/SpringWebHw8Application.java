565de4e03eeb3d7a02dd814cdae437762c40aa72
crangoren <crangoren@gmail.com>
2021-12-09 13:42:43 +0300

    Initial commit

---
+++ spring-web-hw8/src/main/java/rksuleimanov/springwebhw8/SpringWebHw8Application.java
@@ -0,0 +1,13 @@
+package rksuleimanov.springwebhw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebHw8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebHw8Application.class, args);
+	}
+
+}
